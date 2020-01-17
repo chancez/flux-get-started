58a6dfae484cf35d2d096ef14fd40ce2abd1b5a4
Chance Zibolski <czibolski@rigetti.com>
2020-01-17 11:26:33 -0800

    wip

--- loki-jsonnet/jsonnet-flux.sh
+++ loki-jsonnet/jsonnet-flux.sh
@@ -2,9 +2,9 @@
 
 # set -e
 
-# if ! test -f "/usr/local/bin/tk"; then
-#     wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk" > /tmp/tk.log
-#     chmod +x /usr/local/bin/tk
-# fi
+if ! test -f "/usr/local/bin/tk"; then
+    wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk" >/dev/null 2>&1
+    chmod +x /usr/local/bin/tk
+fi
 
-# tk show --dangerous-allow-redirect logging/environments/loki
+tk show --dangerous-allow-redirect logging/environments/loki
