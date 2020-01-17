d8cb6fb4bf3141cd0f2f23dc89c3e7c0aa6e9cc8
Chance Zibolski <czibolski@rigetti.com>
2020-01-17 10:49:33 -0800

    Download tanka

--- loki-jsonnet/jsonnet-flux.sh
+++ loki-jsonnet/jsonnet-flux.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if ! test -f "/usr/local/bin/tk"; then
+    wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk"
+fi
+
 tk show logging/environments/loki
