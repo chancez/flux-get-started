b8beb8da6dd58a3142efed9b8524ffc6f4795a3e
Chance Zibolski <czibolski@rigetti.com>
2020-01-17 11:20:20 -0800

    wip

--- loki-jsonnet/jsonnet-flux.sh
+++ loki-jsonnet/jsonnet-flux.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if ! test -f "/usr/local/bin/tk"; then
-    wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk"
-    chmod +x /usr/local/bin/tk
-fi
+# set -e
 
-tk show logging/environments/loki > flux-patch.yaml
+# if ! test -f "/usr/local/bin/tk"; then
+#     wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk"
+#     chmod +x /usr/local/bin/tk
+# fi
+
+# tk show logging/environments/loki
+cat loki-ns.yaml
