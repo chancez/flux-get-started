6fd45b5d4cec3bc8125e4de5923967bcb4bdada8
Chance Zibolski <czibolski@rigetti.com>
2020-01-17 10:54:41 -0800

    fix permissions of tk

--- loki-jsonnet/jsonnet-flux.sh
+++ loki-jsonnet/jsonnet-flux.sh
@@ -2,6 +2,7 @@
 
 if ! test -f "/usr/local/bin/tk"; then
     wget "https://github.com/sh0rez/tanka/releases/download/v0.6.1/tk-linux-amd64" -O "/usr/local/bin/tk"
+    chmod +x /usr/local/bin/tk
 fi
 
 tk show logging/environments/loki
